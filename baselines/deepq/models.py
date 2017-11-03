384e2565be4d60c907a9d3be1ecde26d738ed3a3
MaximeBouton <maxim.bouton@gmail.com>
2017-11-03 14:31:51 -0700

    added regularizer

--- baselines/deepq/models.py
+++ baselines/deepq/models.py
@@ -30,7 +30,7 @@ def mlp(hiddens=[], layer_norm=False):
     return lambda *args, **kwargs: _mlp(hiddens, layer_norm=layer_norm, *args, **kwargs)
 
 
-def _cnn_to_mlp(convs, hiddens, baseline_policy, dueling, inpt, num_actions, scope, reuse=False, layer_norm=False):
+def _cnn_to_mlp(convs, hiddens, baseline_policy, dueling, inpt, num_actions, scope, reuse=False, layer_norm=False, regularizer=None):
     tf_baseline = True
     with tf.variable_scope(scope, reuse=reuse):
         out = inpt
@@ -40,26 +40,27 @@ def _cnn_to_mlp(convs, hiddens, baseline_policy, dueling, inpt, num_actions, sco
                                            num_outputs=num_outputs,
                                            kernel_size=kernel_size,
                                            stride=stride,
+                                           regularizer=regularizer,
                                            activation_fn=tf.nn.relu)
         conv_out = layers.flatten(out)
         with tf.variable_scope("action_value"):
             action_out = conv_out
             for hidden in hiddens:
-                action_out = layers.fully_connected(action_out, num_outputs=hidden, activation_fn=None)
+                action_out = layers.fully_connected(action_out, num_outputs=hidden, activation_fn=None, regularizer=regularizer)
                 if layer_norm:
                     action_out = layers.layer_norm(action_out, center=True, scale=True)
                 action_out = tf.nn.relu(action_out)
-            action_scores = layers.fully_connected(action_out, num_outputs=num_actions, activation_fn=None)
+            action_scores = layers.fully_connected(action_out, num_outputs=num_actions, activation_fn=None, regularizer=regularizer)
 
         if dueling:
             with tf.variable_scope("state_value"):
                 state_out = conv_out
                 for hidden in hiddens:
-                    state_out = layers.fully_connected(state_out, num_outputs=hidden, activation_fn=None)
+                    state_out = layers.fully_connected(state_out, num_outputs=hidden, activation_fn=None, regularizer=regularizer)
                     if layer_norm:
                         state_out = layers.layer_norm(state_out, center=True, scale=True)
                     state_out = tf.nn.relu(state_out)
-                state_score = layers.fully_connected(state_out, num_outputs=1, activation_fn=None)
+                state_score = layers.fully_connected(state_out, num_outputs=1, activation_fn=None, regularizer=regularizer)
             action_scores_mean = tf.reduce_mean(action_scores, 1)
             action_scores_centered = action_scores - tf.expand_dims(action_scores_mean, 1)
             q_out = state_score + action_scores_centered
@@ -75,7 +76,7 @@ def _cnn_to_mlp(convs, hiddens, baseline_policy, dueling, inpt, num_actions, sco
         return q_out
 
 
-def cnn_to_mlp(convs, hiddens, baseline_policy=None, dueling=False, layer_norm=False):
+def cnn_to_mlp(convs, hiddens, baseline_policy=None, dueling=False, layer_norm=False, regularizer=None):
     """This model takes as input an observation and returns values of all actions.
 
     Parameters
@@ -95,4 +96,4 @@ def cnn_to_mlp(convs, hiddens, baseline_policy=None, dueling=False, layer_norm=F
         q_function for DQN algorithm.
     """
 
-    return lambda *args, **kwargs: _cnn_to_mlp(convs, hiddens, baseline_policy, dueling, layer_norm=layer_norm, *args, **kwargs)
+    return lambda *args, **kwargs: _cnn_to_mlp(convs, hiddens, baseline_policy, dueling, layer_norm=layer_norm, regularizer=regularizer, *args, **kwargs)
